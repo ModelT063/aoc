8dd269b417da3cb72edb95f3d3cd2e95a8e04d8c
ModelT063 <jpittar@clemson.edu>
2021-12-01 09:38:39 -0500

    formatting

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+
 using namespace std;
+
 int main(){
     // PART 1
     ifstream in{"input.txt"};
     int count = 0;
     int cur;
     int prev;
+
     in >> cur;
     while(in){
         prev = cur;
@@ -15,6 +18,7 @@ int main(){
         if(cur > prev) count++;
     }
     in.close();
+    
     cout << "PART 1: " << count << endl;
     return 0;
 }
