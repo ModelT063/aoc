e6b2a5d2fd5a058aa01f7454bfe1da40a18ad451
ModelT063 <jpittar@clemson.edu>
2021-12-02 23:53:10 -0500

    day 3 prep

---
+++ day3/day3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std
+
+int main(){
+	ifstream in("input.txt");
+
+	return 0;
+}
+
+void part1(){
+	return;
+}
+
+void part2(){
+	return;
+}
+	
