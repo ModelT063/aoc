633fc6fc68eaf2daa44603a9828d873cc661bb9a
ModelT063 <jpittar@clemson.edu>
2021-12-12 22:34:19 -0500

    working part 1

--- day9/day9.cpp
+++ day9/day9.cpp
@@ -1,9 +1,40 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 void part1(){
+    ifstream in{"input.txt"};
+    vector<vector<int>> numbers;
+    string temp;
+    int total = 0;
+    // reads data into a 2d vector
+    while(getline(in, temp)){
+        vector<int> vtemp;
+        for(char c: temp) vtemp.push_back(c - 48);
+        numbers.push_back(vtemp);
+    }
+
+    for(auto i: numbers){
+        for(auto j: i){
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    // iterates through each number in the vectorj
+    for(int i = 0; i < numbers.size(); i++){
+        for(int j = 0; j < numbers[i].size(); j++){
+            if(numbers[i][j] < (j < numbers[i].size() - 1? numbers[i][j + 1]: 10) &&
+            numbers[i][j] < (j > 0? numbers[i][j - 1]: 10) &&
+            numbers[i][j] < (i > 0 ? numbers[i - 1][j]: 10) &&
+            numbers[i][j] < (i < numbers[i].size() - 1? numbers[i + 1][j]: 10)) 
+                total += numbers[i][j] + 1;
+        }
+    }
+
+    cout << total << endl;
     return;
 }
 
@@ -12,5 +43,6 @@ void part2(){
 }
 
 int main(){
+    part1();
     return 0;
 }
