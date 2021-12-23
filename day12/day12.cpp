adcb002b6ce8b058fd0d3369036ecbcb0a7ccc39
ModelT063 <jpittar@clemson.edu>
2021-12-22 22:07:24 -0500

    random commits

---
+++ day12/day12.cpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+void part1(){
+    ifstream in{"input.txt"};
+    map<string, vector<string>>
+
+}
+
+void part2(){
+
+
+}
+
+int main(){
+
+    return 0;
+}
